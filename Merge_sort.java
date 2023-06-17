6cf6273a808be3ffc4d6aa13c3c03784066640be
Pritam Samui <77156502+Pritamart@users.noreply.github.com>
2023-06-17 14:55:43 +0530

    Create Merge_sort.java

---
+++ Merge_sort.java
@@ -0,0 +1,61 @@
+//date - 17 jun 2023
+//marge sort 
+// java
+public class Merge_sort {
+  //divided the array 
+    public void sort(int[] arr, int left, int right) {
+        if (left < right) {
+            int mid = left + (right - left) / 2;
+            sort(arr, left, mid);
+            sort(arr, mid + 1, right);
+            merge(arr, left, mid, right);
+        }
+    }
+  //merge the array 
+    public void merge(int[] arr, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+
+        for (int i = 0; i < n1; i++)
+            L[i] = arr[left + i];
+        for (int j = 0; j < n2; j++)
+            R[j] = arr[mid + 1 + j];
+
+        int i = 0, j = 0;
+        int k = left;
+
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {12, 11, 13, 5, 6, 7};
+
+        Merge_sort ob = new Merge_sort();
+        ob.sort(arr, 0, arr.length - 1);
+        for (int i = 0; i < arr.length; ++i)
+			System.out.print(arr[i] + " ");
+		System.out.println();
+    }
+}
